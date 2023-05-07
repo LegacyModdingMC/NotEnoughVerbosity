8061c525c609a618cef2dabd42fe8c3d9ddc70e3
makamys <makamys@outlook.com>
2023-05-07 15:14:01 +0200

    Init project

--- src/main/java/io/github/legacymoddingmc/notenoughverbosity/Notenoughverbosity.java
+++ src/main/java/io/github/legacymoddingmc/notenoughverbosity/Notenoughverbosity.java
@@ -1,14 +1,14 @@
-package ${package};
+package io.github.legacymoddingmc.notenoughverbosity;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ${mainclass}.MODID, version = ${mainclass}.VERSION)
-public class ${mainclass}
+@Mod(modid = Notenoughverbosity.MODID, version = Notenoughverbosity.VERSION)
+public class Notenoughverbosity
 {
-    public static final String MODID = "${modid}";
+    public static final String MODID = "notenoughverbosity";
     public static final String VERSION = "@VERSION@";
 
     @EventHandler
